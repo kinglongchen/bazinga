fa105f95d17d5fa006b5fc0017e1d2d53333c3e2
chenjinlong <jinlong.chen@tqmall.com>
2015-08-02 04:08:36 +0800

    添加了几台测试虚拟机

--- connvm.sh
+++ connvm.sh
@@ -8,6 +8,7 @@ if [ ! -f ${configfile} ];then
 	exit 1	
 fi 
 D_USER=root
+D_PORT=22
 D_PW=Max6and7
 AUTH_FILE=~/.ssh/alivm_rsa
 if [ ! -f $AUTH_FILE ];then
@@ -19,8 +20,9 @@ while read line
 	do
 		hostname[${i}]=$(echo $line| awk -F: '{gsub(" |\t","",$1); print $1}')
 		hostip[${i}]=$(echo $line| awk -F: '{gsub(" |\t","",$2); print $2}')
-		username[${i}]=$(echo $line| awk -F: '{gsub(" |\t","",$3); print $3}')
-		passwd[${i}]=$(echo $line| awk -F: '{gsub(" |\t","",$4); print $4}')
+		port[${i}]=$(echo $line| awk -F: '{gsub(" |\t","",$2); print $3}')
+		username[${i}]=$(echo $line| awk -F: '{gsub(" |\t","",$3); print $4}')
+		passwd[${i}]=$(echo $line| awk -F: '{gsub(" |\t","",$4); print $5}')
 		i=$((${i}+1))
 	done<${configfile}
 hostnum=${#hostname[@]}
@@ -57,6 +59,7 @@ while [ $select -lt 1 -o $select -gt $hostnum ]
 				}')
 	done
 HOST=${hostip[$select]}
+PORT=${port[$select]:=$D_PORT}
 USER=${username[$select]:=$D_USER}
 PW=${passwd[$select]:=$D_PW}
-ssh -i $AUTH_FILE $USER@$HOST
+ssh -v -i $AUTH_FILE $USER@$HOST -p $PORT
--- vmlist.ls
+++ vmlist.ls
@@ -1,2 +1,3 @@
 vm1_hz:120.26.56.174
-alivm2:198.11.182.25
+tqmalltest:112.124.66.38:22222:tqmall.scm:test.2015
+gatewaytest:115.29.220.170:22222:test_gateway:gateway.2015
